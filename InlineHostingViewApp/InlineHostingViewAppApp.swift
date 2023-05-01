9d6b442dc9f9a0eb345fdd0d7970da401c4a3929
Stephan Casas <stephancasas@icloud.com>
2023-04-30 23:43:57 -0500

    Initial Commit

---
+++ InlineHostingViewApp/InlineHostingViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InlineHostingViewAppApp.swift
+//  InlineHostingViewApp
+//
+//  Created by Stephan Casas on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct InlineHostingViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
